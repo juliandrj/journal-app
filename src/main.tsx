d6afecb2fe6383e47eb146fb64d520569273a27d
Julian Rojas <jrojas@ditech.es>
2025-04-02 16:57:37 +0200

    Se agrega Redux

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,15 @@ import '@fontsource/roboto/700.css';
 import './styles.css'
 import { JournalApp } from './JournalApp'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <JournalApp />
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <JournalApp />
+            </BrowserRouter>
+        </Provider>
     </StrictMode>,
 )
