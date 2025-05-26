723cd2ac0145beec62421eda750e014530c78e95
Julian Rojas <jrojas@ditech.es>
2025-05-26 11:42:57 +0200

    No se debe llamar directamente al useDispatch, se debe hacer por el useAppDispatch, por temas de tipado.

--- src/hooks/useCheckAuth.tsx
+++ src/hooks/useCheckAuth.tsx
@@ -1,15 +1,15 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { FirebaseAuth } from "../firebase/config";
 import { login, logout } from "../store/auth";
 import { User } from "../interfaces";
-import { RootState } from "../store";
+import { RootState, useAppDispatch } from "../store";
 import { startLoadingNotes } from "../store/journal";
+import { useSelector } from "react-redux";
 
 export const useCheckAuth = () => {
     const {status} = useSelector((state: RootState) => state.auth);
-    const dispatch = useDispatch();
+    const dispatch = useAppDispatch();
 
     useEffect(() => {
         onAuthStateChanged(FirebaseAuth, async (user) => {
